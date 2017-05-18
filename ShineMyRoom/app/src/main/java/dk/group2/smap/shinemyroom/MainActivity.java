7d99740aa98dc98df3a454105bb5c4eafc35b23d
Ao Li <liao@live.dk>
2017-05-18 13:34:59 +0200

    new project

---
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.group2.smap.shinemyroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        
+    }
+}
