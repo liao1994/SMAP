f5db97fa9eb08317912333b9d923d5716b0412ac
Ao Li <liao@live.dk>
2017-05-23 15:35:26 +0200

    listview

---
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/generated/Room.java
@@ -0,0 +1,22 @@
+package dk.group2.smap.shinemyroom.generated;
+
+/**
+ * Created by liao on 23-05-2017.
+ */
+
+public class Room {
+
+    public Room(){}
+    public Room(boolean on){
+        this.on = on;
+    }
+    private Boolean on;
+
+    public Boolean getOn() {
+        return on;
+    }
+
+    public void setOn(Boolean on) {
+        this.on = on;
+    }
+}
