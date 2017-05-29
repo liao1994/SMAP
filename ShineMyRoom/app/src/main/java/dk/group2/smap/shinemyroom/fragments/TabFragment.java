af69260864b776d4840e6ce3b18647b223519712
Msknudsen <m_s_knudsen@hotmail.com>
2017-05-29 20:18:44 +0200

    ,.,

--- ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/fragments/TabFragment.java
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/fragments/TabFragment.java
@@ -1,6 +1,9 @@
 package dk.group2.smap.shinemyroom.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabItem;
 import android.view.LayoutInflater;
@@ -10,14 +13,18 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import dk.group2.smap.shinemyroom.R;
+import dk.group2.smap.shinemyroom.activities.LampActivity;
+import dk.group2.smap.shinemyroom.activities.MainActivity;
 
 public class TabFragment extends Fragment {
+    private static final int HOMETAB_REQUEST_CODE = 1;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View inflate;
-        
+
         inflate = inflater.inflate(R.layout.tabmenu, container, false);
 
         ImageView homeBtn = (ImageView) inflate.findViewById(R.id.homeView);
@@ -26,13 +33,15 @@ public class TabFragment extends Fragment {
             public void onClick(View v) {
                 Toast.makeText(getActivity().getApplicationContext(),"HomeBtn clicked",Toast.LENGTH_SHORT).show();
             }
+
+
         });
 
         ImageView editBtn = (ImageView) inflate.findViewById(R.id.editView);
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(),"EditBtn clicked",Toast.LENGTH_SHORT).show();
+
             }
         });
 
