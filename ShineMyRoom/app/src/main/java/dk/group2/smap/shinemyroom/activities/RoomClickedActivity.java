837670824e9165862294f6056a864a01eebb693d
Msknudsen <m_s_knudsen@hotmail.com>
2017-05-30 21:32:05 +0200

    kokok

--- ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/activities/RoomClickedActivity.java
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/activities/RoomClickedActivity.java
@@ -3,9 +3,24 @@ package dk.group2.smap.shinemyroom.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHBridgeResourcesCache;
+import com.philips.lighting.model.PHLight;
+
+import java.util.List;
+
 import dk.group2.smap.shinemyroom.R;
 
 public class RoomClickedActivity extends AppCompatActivity {
+
+    PHBridge phbridge = PHHueSDK.getInstance().getSelectedBridge();
+    PHBridgeResourcesCache resourceCache = phbridge.getResourceCache();
+
+    List<PHLight> allLights = PHHueSDK.getInstance()
+            .getSelectedBridge()
+            .getResourceCache()
+            .getAllLights();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/storage/RoomClickedAdapter.java
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/storage/RoomClickedAdapter.java
@@ -13,10 +13,12 @@ import android.widget.TextView;
 
 import com.philips.lighting.hue.sdk.PHHueSDK;
 import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHBridgeResourcesCache;
 import com.philips.lighting.model.PHLight;
 import com.philips.lighting.model.PHLightState;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 import dk.group2.smap.shinemyroom.Global;
@@ -24,21 +26,21 @@ import dk.group2.smap.shinemyroom.R;
 import dk.group2.smap.shinemyroom.RoomAdapter;
 import dk.group2.smap.shinemyroom.generated.Room;
 
-public class RoomClickedAdapter extends ArrayAdapter<Room> {
+public class RoomClickedAdapter extends ArrayAdapter<PHLight> {
     private boolean sleeping;
-    private ArrayList<Room> editrooms;
+    private ArrayList<PHLight> editlights;
     private Context c;
 
-    public RoomClickedAdapter(Context c, ArrayList<Room> editrooms) {
+    public RoomClickedAdapter(Context c, ArrayList<PHLight> editrooms) {
         super(c, R.layout.item_edit_room_list, editrooms);
         this.c = c;
-        this.editrooms = editrooms;
+        this.editlights = editlights;
     }
 
     @Override
-    public Room getItem(int position) {
-        if (editrooms != null) {
-            return editrooms.get(position);
+    public PHLight getItem(int position) {
+        if (editlights != null) {
+            return editlights.get(0);
         } else {
             return null;
         }
@@ -51,13 +53,18 @@ public class RoomClickedAdapter extends ArrayAdapter<Room> {
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        View view;
+        PHLightState LampState = editlights.get(position).getLastKnownLightState();
+        String LampStateString = Boolean.toString(LampState.isOn());
+
         if (convertView == null) {
             LayoutInflater inflator = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflator.inflate(R.layout.item_room_list, null);
         }
         TextView LampName = (TextView) convertView.findViewById(R.id.room_clicked_lamp_name);
-        //LampName.setText();
+        LampName.setText(editlights.get(position).getName());
+        TextView LampStatus = (TextView) convertView.findViewById(R.id.room_clicked_lamp_status);
+        LampStatus.setText(LampStateString);
+
         return convertView;
     }
 }
