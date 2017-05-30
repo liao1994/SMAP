a983277a05dca5701aabb363669adbbffcd603fd
Ao Li <liao@live.dk>
2017-05-30 17:06:35 +0200

    supa!!

--- ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/FragmentPageAdapter.java
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/FragmentPageAdapter.java
@@ -11,6 +11,9 @@ import android.text.SpannableString;
 import android.text.style.ImageSpan;
 import android.util.SparseArray;
 import android.view.MotionEvent;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,6 +28,7 @@ public class FragmentPageAdapter extends FragmentStatePagerAdapter {
 //    private ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
 //    private ArrayList<String> fragmentTitleArrayList = new ArrayList<>();
     private SparseArray<Fragment> fragmentMap = new SparseArray<>();
+    private SparseArray<String> tagMap = new SparseArray<>();
     private HashMap<Class<? extends Fragment>,Integer> fragmentToKey = new HashMap<>();
     private ArrayList<Fragment> fragments = new ArrayList<>();
     private Context c;
@@ -70,12 +74,31 @@ public class FragmentPageAdapter extends FragmentStatePagerAdapter {
             return;
         }
         fragmentMap.append(key, fragment);
+        this.notifyDataSetChanged();
 
     }
     @Override
     public Fragment getItem(int position) {
         return fragmentMap.get(position);
     }
+    SparseArray<View> views = new SparseArray<View>();
+
+//    @Override
+//    public Object instantiateItem(ViewGroup container, int position) {
+//        Object o = super.instantiateItem(container, position);
+//        if (o instanceof Fragment){
+//            Fragment f = (Fragment) o;
+//            String tag = f.getTag();
+//            tagMap.put(position,tag);
+//        }
+//        return o;
+//    }
+
+    //    //https://stackoverflow.com/questions/7263291/viewpager-pageradapter-not-updating-the-view
+//    @Override
+//    public int getItemPosition(Object object) {
+//        return POSITION_NONE;
+//    }
 
     @Override
     public int getCount() {
