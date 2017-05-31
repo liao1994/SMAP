bdb16609d78847c79eb2184317308b3c98504835
Msknudsen <m_s_knudsen@hotmail.com>
2017-05-31 14:18:12 +0200

    jg

--- ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/activities/AddRoomActivity.java
+++ ShineMyRoom/app/src/main/java/dk/group2/smap/shinemyroom/activities/AddRoomActivity.java
@@ -2,6 +2,8 @@ package dk.group2.smap.shinemyroom.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import dk.group2.smap.shinemyroom.R;
 
@@ -11,5 +13,24 @@ public class AddRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_room);
+
+
+
+
+
+        Button saveBtn = (Button)findViewById(R.id.room_add_saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Button cancelBtn = (Button)findViewById(R.id.room_add_cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
